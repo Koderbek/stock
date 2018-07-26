e425b5372c2fa724dc1306c37cd8fd1cca5cf6c8
Yuriy Kurnosov <kurnosov98@yandex.ru>
2018-07-26 21:20:59 +0400

    first commit

---
+++ src/AppBundle/Entity/Porter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Юрий
+ * Date: 26.07.2018
+ * Time: 21:15
+ */
+
+namespace AppBundle\Entity;
+
+
+class Porter
+{
+
+}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,215 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergey
+ * Date: 21.06.2018
+ * Time: 15:53
+ */
+
+namespace AppBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="users")
+ */
+abstract class User implements UserInterface
+{
+    /**
+     * @var integer
+     * @ORM\Column(type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     *
+     * @Assert\NotBlank(message="Email обязателен")
+     * @Assert\Email(message="Некорректный Email")
+     */
+    protected $email;
+
+    /**
+     * @var string
+     * @ORM\Column(name="role", type="string")
+     *
+     * @Assert\NotBlank(message="Введите роль")
+     */
+    protected $role;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     *
+     * @Assert\NotBlank(message="Введите пароль")
+     * @Assert\Length(
+     *     min=8,
+     *     max=20,
+     *     minMessage="Ваш пароль должен быть как минимум {{ limit }} символов",
+     *     maxMessage="Ваш пароль не должен быть длиннее {{ limit }} символов"
+     * )
+     */
+    protected $password;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     *
+     * @Assert\NotBlank(message="Заполните поле")
+     */
+    protected $salt;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     *
+     * @Assert\NotBlank(message="Заполните поле имя")
+     */
+    protected $name;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     *
+     * @Assert\NotBlank(message="Заполните поле фамилия")
+     */
+    protected $surname;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param string $role
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * @param string $surname
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * @param string $salt
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+    }
+
+
+    public function getRoles()
+    {
+        return [$this->getRole()];
+    }
+
+    public function getUsername()
+    {
+        return $this->getEmail();
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function __toString()
+    {
+        return (string)$this->getName() . ' ' . $this->getSurname();
+    }
+}
